61d980e712b899bed7007cd9c89a569e757b0147
Izzet Mustsfaiev <webdizz@gmail.com>
2014-02-18 17:18:57 +0200

    Add import of tasks during deploymen

--- todo/src/main/resources/import.sql
+++ todo/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 -- You can use this file to load seed data into the database using SQL statements
 
 INSERT INTO users (id, login, password) VALUES (1, 'webdizz@gmail.com', '123456');
+
+INSERT INTO tasks (id, name, due_datetime) VALUES (1, 'Sample quite urgent task to do!', CURRENT_TIMESTAMP + 2);
