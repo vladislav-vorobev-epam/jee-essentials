b11550896619f385c40d1416f8d5be66275b26aa
Izzet Mustsfaiev <webdizz@gmail.com>
2014-02-26 23:53:39 +0200

    Remove not needed for now beans

--- todo/src/main/java/com/epam/cdp/jee/todo/rest/JaxRsActivator.java
+++
@@ -1,9 +0,0 @@
-package com.epam.cdp.jee.todo.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class JaxRsActivator extends Application {
-    /* class body intentionally left blank */
-}
--- todo/src/main/java/com/epam/cdp/jee/todo/soap/client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://soap.todo.jee.cdp.epam.com/")
-package com.epam.cdp.jee.todo.soap.client;
