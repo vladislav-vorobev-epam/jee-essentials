e51c53f8e83a2b2a8377de25a1740fc595ad3f73
Izzet Mustsfaiev <webdizz@gmail.com>
2014-07-08 01:24:45 +0300

    Add chown for /opt directory

--- chef/cookbooks/jee/recipes/jboss.rb
+++ chef/cookbooks/jee/recipes/jboss.rb
@@ -7,6 +7,7 @@ bash "download JBoss" do
   cwd '/opt'
   code <<-EOF
     wget -c http://webdizz.name/jeeessentials/jboss-eap-6.2.0.zip
+    chown -R vagrant:vagrant /opt
   EOF
   not_if { ::File.exists?('/opt/jboss-eap-6.2.0.zip') }
 end
